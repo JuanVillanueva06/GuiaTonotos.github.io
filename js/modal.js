aac6046117c3162d44931b9453a5d1d807e45242
JuanVillanueva06 <juan.villanueva.yk87@gmail.com>
2026-07-08 01:43:42 -0500

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,72 @@
+$(function(){
+
+    $("#mostrar_INICIO").click(function(){
+    $("#fondo_INICIO").fadeIn();
+    });
+    $("#cerrar_INICIO").click(function(){
+        $("#fondo_INICIO").fadeOut();
+    });
+
+    $("#fondo_Brainstorming").fadeOut();
+    $("#mostrar_Brainstorming").click(function(){
+        $("#fondo_Brainstorming").fadeIn();
+    });
+    $("#cerrar_Brainstorming").click(function(){
+        $("#fondo_Brainstorming").fadeOut();
+    });
+
+    $("#fondo_DesignThinking").fadeOut();
+    $("#mostrar_DesignThinking").click(function(){
+        $("#fondo_DesignThinking").fadeIn();
+    });
+    $("#cerrar_DesignThinking").click(function(){
+        $("#fondo_DesignThinking").fadeOut();
+    });
+
+        $("#fondo_SCAMPER").fadeOut();
+    $("#mostrar_SCAMPER").click(function(){
+        $("#fondo_SCAMPER").fadeIn();
+    });
+    $("#cerrar_SCAMPER").click(function(){
+        $("#fondo_SCAMPER").fadeOut();
+    });
+
+
+
+
+    $("#fondo_eno").fadeOut();
+    $("#mostrar_eno").click(function(){
+        $("#fondo_eno").fadeIn();
+    });
+    $("#cerrar_eno").click(function(){
+        $("#fondo_eno").fadeOut();
+    });
+    
+    $("#fondo_AlphaGenome").fadeOut();
+    $("#mostrar_AlphaGenome").click(function(){
+        $("#fondo_AlphaGenome").fadeIn();
+    });
+    $("#cerrar_AlphaGenome").click(function(){
+        $("#fondo_AlphaGenome").fadeOut();
+    });
+    
+    $("#fondo_ForestCity").fadeOut();
+    $("#mostrar_ForestCity").click(function(){
+        $("#fondo_ForestCity").fadeIn();
+    });
+    $("#cerrar_ForestCity").click(function(){
+        $("#fondo_ForestCity").fadeOut();
+    });
+    
+    $("#fondo_ProyectosSociales").fadeOut();
+    $("#mostrar_ProyectosSociales").click(function(){
+        $("#fondo_ProyectosSociales").fadeIn();
+    });
+    $("#cerrar_ProyectosSociales").click(function(){
+        $("#fondo_ProyectosSociales").fadeOut();
+    });
+    
+
+    
+    
+});
